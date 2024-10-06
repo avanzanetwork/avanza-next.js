6f1e026459825e21f02fdaf7f43c274db31002ec
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2024-10-06 14:49:56 -0500

    gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+    output: 'export',
+    assetPrefix: "/",
+    images: {
+      unoptimized: true,
+    },
+};
